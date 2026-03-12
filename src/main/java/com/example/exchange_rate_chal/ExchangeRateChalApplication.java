8cf0a63bd851c1a01ebf34902005b3974a2f47a6
Rodrigo <cruz.rodrigo98@gmail.com>
2026-03-12 13:32:42 +0000

    Spring Boot Base

---
+++ src/main/java/com/example/exchange_rate_chal/ExchangeRateChalApplication.java
@@ -0,0 +1,13 @@
+package com.example.exchange_rate_chal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateChalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateChalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exchange_rate_chal/ExchangeRateChalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exchange_rate_chal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateChalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
